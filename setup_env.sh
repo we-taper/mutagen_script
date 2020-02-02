39eea8c789f4c2047577647842a8a1118e60ae74
Hongxiang Chen <h.chen.17@ucl.ac.uk>
2020-02-02 12:39:37 +0000

    update

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 conda create -n mutagen python=3.6
 source ~/miniconda3/bin/activate
 conda activate mutagen
-conda install -c conda-forge mutagen
-conda install --yes autopep8 ipython rope pylint
+conda install -n mutagen --yes -c conda-forge mutagen
+conda install -n mutagen --yes autopep8 ipython rope pylint
